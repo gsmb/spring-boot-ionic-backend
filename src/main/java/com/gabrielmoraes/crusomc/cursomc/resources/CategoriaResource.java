d0b0d0f87309eed34362d3dcbd8e465af3787fcc
Gabriel Moraes <gabrielbar62@gmail.com>
2019-04-24 21:18:06 -0300

    Listando todas categorias e implementação de classe DTO

--- src/main/java/com/gabrielmoraes/crusomc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gabrielmoraes/crusomc/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.gabrielmoraes.crusomc.cursomc.resources;
 
 import com.gabrielmoraes.crusomc.cursomc.domain.Categoria;
+import com.gabrielmoraes.crusomc.cursomc.dto.CategoriaDTO;
 import com.gabrielmoraes.crusomc.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "categorias")
@@ -39,5 +42,12 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> delete(@PathVariable Integer id) {
         categoriaService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list = categoriaService.findAll();
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body((listDto));
     }
 }
