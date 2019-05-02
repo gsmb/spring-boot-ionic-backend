e9d569980ad5f8232a0efb16b93847a69fc1dad8
Gabriel Moraes <gabrielbar62@gmail.com>
2019-05-02 20:03:53 -0300

    Personalizando o retorno da validação

--- src/main/java/com/gabrielmoraes/crusomc/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/gabrielmoraes/crusomc/cursomc/resources/exceptions/StandardError.java
@@ -3,6 +3,7 @@ package com.gabrielmoraes.crusomc.cursomc.resources.exceptions;
 import java.io.Serializable;
 
 public class StandardError implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private Integer status;
     private String msg;
